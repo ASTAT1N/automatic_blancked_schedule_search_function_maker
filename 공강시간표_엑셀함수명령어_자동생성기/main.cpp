eacf74764beec56c99a2ea2126798a437a2e907c
Huisu Lim <4922865@gmail.com>
2019-08-28 02:11:30 +0900

    change it as using vector and class

--- 공강시간표_엑셀함수명령어_자동생성기/main.cpp
+++ 공강시간표_엑셀함수명령어_자동생성기/main.cpp
@@ -1,44 +1,59 @@
 #include<iostream>
 #include<string>
+#include<vector>
+class data{
+public:
+	data(char _name[6]){
+		int ms = strlen(_name);
+		//월요일 1교시 위치 찾기
+		loc[0] = _name[0] + 2;
+		loc[ms - 1] = (_name[ms - 1] + 2);
+		bool sw = (loc[ms - 1] >= 10);
+		for (int i = ms - 2; i > 0; --i) {
+			if (sw) {
+				loc[i + 1] %= 10;
+				loc[i] = _name[i] + 1;
+				if (loc[i + 1] < 10) {
+					sw = false;
+				}
+			}
+			else {
+				loc[i] = _name[i];
+			}
+		}
+		loc[ms] = NULL;
+		//이름위치 고정화
+		for (int i = ms; i > 0; --i) {
+			name[i + 2] = _name[i];
+		}
+		name[1] = _name[0];
+		name[0] = '$';
+		name[2] = '$';
+	}
+	char* get_name() {
+		return name;
+	}
+	char* get_loc() {
+		return loc;
+	}
+private:
+	char name[8];
+	char loc[6];
+};
 using namespace std;
 int main() {
-	int MX, i, j,ms;
-	char name[20][8], loc[20][6];
+	int MX,i;
+	char name[6];
+	vector<class::data>con;
 	//입력
 	cout << "인원수를 입력해 주세요.";
 	cin >> MX;
 	cout << "이름의 좌표값을 입력해 주세요\n";
 	for (i = 0; i < MX; i++) {
-		cin >> name[i];
+		cin >> name;
+		con.push_back(name);
+		cout << con[i].get_name() << " " << con[i].get_loc() << "\n";
 	}
-	for ( i = 0; i < MX; i++){
-		ms = strlen(name[i]);
-		//월요일 1교시 위치 찾기
-		loc[i][0] = name[i][0] + 2;
-		loc[i][ms - 1] = (name[i][ms - 1] + 2);
-		bool sw=(loc[i][ms-1]>=10);
-		for (j = ms-2; j>0; --j) {
-			if (sw) {
-				loc[i][j] = name[i][j] + 1;
-				if (loc[i][j + 1] < 10) {
-					sw = false;
-				}
-			}
-			else {
-				loc[i][j] = name[i][j];
-			}
-			
-			loc[i][j] = name[i][j] + ((name[i][j + 1] + 2));
-		}
-		//이름위치 고정화
-		for (j = ms; j >= 0; --j) {
-			name[i][j+2] = name[i][j];
-		}
-		name[i][1] = name[i][2];
-		name[i][0] = '$';
-		name[i][2] = '$';
-	}
-	
 	//출력
 	//(IF(loc=0,name,"")&",")
 	cout << "=";
