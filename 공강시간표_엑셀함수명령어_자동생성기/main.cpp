6cc00d7cf2dffaa19e81669624a6e1d3b104a682
Huisu Lim <4922865@gmail.com>
2019-08-27 12:31:38 +0900

    Making input function and insert excel commend by remark

--- 공강시간표_엑셀함수명령어_자동생성기/main.cpp
+++ 공강시간표_엑셀함수명령어_자동생성기/main.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
+#include<string>
+using namespace std;
 int main() {
 	int MX, i;
+	char name[20][6], loc[20][6];
+	//입력
+	cout << "인원수를 입력해 주세요.";
+	cin >> MX;
+	cout << "이름과 월요일 1교시의 좌표값을 입력해 주세요\n";
+	for (i = 0; i < MX; i++){
+		cin >> name[i] >> loc[i];
+	}
+	//출력
+	//(IF(loc=0,name,"")&",")
+	cout << "=";
+
 	return 0;
 }
