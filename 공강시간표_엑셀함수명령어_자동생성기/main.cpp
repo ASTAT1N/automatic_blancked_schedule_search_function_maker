a1bcbeb0bc0532465829ccf41eb2195487bb8eaa
Huisu Lim <4922865@gmail.com>
2019-08-28 01:38:22 +0900

    Pointing monday 1st class by using position of name

--- 공강시간표_엑셀함수명령어_자동생성기/main.cpp
+++ 공강시간표_엑셀함수명령어_자동생성기/main.cpp
@@ -2,19 +2,36 @@
 #include<string>
 using namespace std;
 int main() {
-	int MX, i,j;
+	int MX, i, j,ms;
 	char name[20][8], loc[20][6];
 	//입력
 	cout << "인원수를 입력해 주세요.";
 	cin >> MX;
-	cout << "이름과 월요일 1교시의 좌표값을 입력해 주세요\n";
-	for (i = 0; i < MX; i++){
-		cin >> name[i] >> loc[i];
+	cout << "이름의 좌표값을 입력해 주세요\n";
+	for (i = 0; i < MX; i++) {
+		cin >> name[i];
 	}
-	//이름위치 고정화
-	for ( i = 0; i < MX; i++)
-	{
-		for (j = strlen(name[i]); j >= 0; --j) {
+	for ( i = 0; i < MX; i++){
+		ms = strlen(name[i]);
+		//월요일 1교시 위치 찾기
+		loc[i][0] = name[i][0] + 2;
+		loc[i][ms - 1] = (name[i][ms - 1] + 2);
+		bool sw=(loc[i][ms-1]>=10);
+		for (j = ms-2; j>0; --j) {
+			if (sw) {
+				loc[i][j] = name[i][j] + 1;
+				if (loc[i][j + 1] < 10) {
+					sw = false;
+				}
+			}
+			else {
+				loc[i][j] = name[i][j];
+			}
+			
+			loc[i][j] = name[i][j] + ((name[i][j + 1] + 2));
+		}
+		//이름위치 고정화
+		for (j = ms; j >= 0; --j) {
 			name[i][j+2] = name[i][j];
 		}
 		name[i][1] = name[i][2];
