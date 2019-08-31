493d3bbe9205be2377d1a678c95688923c948b28
Huisu Lim <4922865@gmail.com>
2019-09-01 01:54:30 +0900

    finding day-blank schedule

--- 공강시간표_엑셀함수명령어_자동생성기/main.cpp
+++ 공강시간표_엑셀함수명령어_자동생성기/main.cpp
@@ -6,10 +6,10 @@ public:
 	data(char _name[6]){
 		int ms = strlen(_name);
 		//월요일 1교시 위치 찾기
-		int addMon = 3;
+		int addMon = 3,addBlank=2;
 		loc[0] = _name[0] + 2;
-		bool sw = ((mInt(&_name[ms - 1])+3) >= 10);
-		loc[ms - 1] = (_name[ms - 1] + 3);
+		bool sw = ((mInt(&_name[ms - 1])+addMon) >= 10);
+		loc[ms - 1] = (_name[ms - 1] + addMon);
 		for (int i = ms - 2; i > 0; --i) {
 			if (sw) {
 				loc[i + 1] -= 10;
@@ -31,6 +31,32 @@ public:
 			loc[1] = '1';
 		}
 		loc[ms+((int)sw)] = NULL;
+		//공강찾기
+		blank[0] = _name[0] + 2;
+		blank[1] = '$';
+		sw = ((mInt(&_name[ms - 1]) + addBlank) >= 10);
+		blank[ms] = (_name[ms - 1] + addBlank);
+		for (int i = ms - 2; i > 0; --i) {
+			if (sw) {
+				blank[i + 2] -= 10;
+				if (mInt(&_name[i]) != 9) {
+					sw = false;
+				}
+				blank[i+1] = _name[i] + 1;
+
+			}
+			else {
+				blank[i+1] = _name[i];
+			}
+		}
+		if (sw) {
+			blank[2] -= 10;
+			for (int i = ms+1; i > 1; --i) {
+				blank[i] = blank[i - 1];
+			}
+			blank[2] = '1';
+		}
+		blank[ms +1+ ((int)sw)] = NULL;
 		//이름위치 고정화
 		for (int i = ms; i > 0; --i) {
 			name[i + 2] = _name[i];
@@ -48,13 +74,16 @@ public:
 	char* get_loc() {
 		return loc;
 	}
+	char* get_blank() {
+		return blank;
+	}
 private:
 	char name[8]="";
-	char loc[6]="";
+	char loc[6]="",blank[7]="";
 };
 using namespace std;
 int main() {
-	int MX,i;
+	int i;
 	char name[6];
 	vector<class::data>con;
 	//입력
@@ -63,13 +92,13 @@ int main() {
 	while(cin >> name) {
 		
 		con.push_back(name);
-		//cout << con[i].get_name() << " " << con[i].get_loc() << "\n";
+		//cout << con[0].get_name() << " " << con[0].get_blank() << "\n";
 	}
 	//출력
-	//(IF(loc=0,name,"")&",")
+	//(IF(AND(loc=0,blank<24),name&",","")&)
 	cout << "\n시간표: =(";
 	for (i = 0; i < con.size(); ++i) {
-		cout << "IF(" << con[i].get_loc() << "=0," << con[i].get_name() << "&\",\",\"\")";
+		cout << "IF(AND(" << con[i].get_loc() << "=0,"<<con[i].get_blank()<<"<24)," << con[i].get_name() << "&\",\",\"\")";
 		if (i < (con.size() - 1)) {
 			cout << "&";
 		}
@@ -77,7 +106,16 @@ int main() {
 			cout << ")";
 		}
 	}
+	//(IF(blank=24,name&",","")&)
 	cout << "\n공강확인: =(";
-
+	for (i = 0; i < con.size(); ++i) {
+		cout << "IF(" << con[i].get_blank()<<"=24,"<<con[i].get_name()<<"&\",\",\"\")";
+		if (i < (con.size() - 1)) {
+			cout << "&";
+		}
+		else {
+			cout << ")";
+		}
+	}
 	return 0;
 }
